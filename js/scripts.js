752c3547d4562b514ed629df6f4b0d426395cd3c
keitmark <keitmark@mail.ru>
2019-05-03 15:51:09 +0400

    feat: add responsive

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,25 @@ $(document).ready(function(){
         arrows: false,
         speed: 500,
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true
+              }
+            }
+        ]
+    });
+
+    $('.sm-butt-menu').click(function(){
+        $('.menu').slideDown(300);
+    });
+    
+    $('.menu__close-menu').click(function(){
+        $('.menu').slideUp(300);
     });
 
 });
